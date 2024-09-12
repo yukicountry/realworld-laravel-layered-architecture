6917f8baf9390a5c83e0d075616c95ec39066013
Yuki Takada <yuki.takada@ytakada.dev>
2024-09-12 22:37:01 +0900

    implement get profile api

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,12 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Queries\Services\ProfileQueryService;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
-class ProfileController extends Controller
+final class ProfileController extends Controller
 {
-    public function getProfile(): JsonResponse
+    public function getProfile(ProfileQueryService $queryService, Request $request, string $username): JsonResponse
     {
-        return new JsonResponse();
+        $profile = $queryService->getProfileByUsername($username, $request->user());
+
+        if (is_null($profile)) {
+            throw new NotFoundHttpException("Profile of {$username} could not be found.");
+        }
+
+        return new JsonResponse(['profile' => $profile]);
     }
 }
--- app/Queries/Services/ProfileQueryService.php
+++ app/Queries/Services/ProfileQueryService.php
@@ -33,4 +33,31 @@ final class ProfileQueryService
 
         return array_map($mapToModel, $dtos);
     }
+
+    public function getProfileByUsername(string $username, ?string $currentUserId): ?Profile
+    {
+        $dto = DB::table('users')
+            ->select([
+                'users.*',
+                DB::raw('
+                    CASE
+                        WHEN follows.followee_id IS NULL THEN FALSE
+                        ELSE TRUE
+                    END AS following
+                '),
+            ])
+            ->leftJoin('follows', function (JoinClause $join) use ($currentUserId) {
+                $join
+                    ->on('users.id', '=', 'followee_id')
+                    ->where('follows.follower_id', $currentUserId);
+            })
+            ->where('users.username', $username)
+            ->first();
+
+        if (is_null($dto)) {
+            return null;
+        }
+
+        return new Profile($dto->username, $dto->bio, $dto->image, $dto->following);
+    }
 }
