ea0d3c65bf76e7b6cb5a241d2c3d1da7e848447a
Yuki Takada <yuki.takada@ytakada.dev>
2024-09-14 02:13:32 +0900

    add cors settings

---
+++ config/cors.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cross-Origin Resource Sharing (CORS) Configuration
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure your settings for cross-origin resource sharing
+    | or "CORS". This determines what cross-origin operations may execute
+    | in web browsers. You are free to adjust these settings as needed.
+    |
+    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+    |
+    */
+
+    'paths' => ['api/*'],
+
+    'allowed_methods' => explode(',', env('CORS_ALLOWED_METHODS', '')),
+
+    'allowed_origins' => explode(',', env('CORS_ALLOWED_ORIGINS', '')),
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => explode(',', env('CORS_ALLOWED_HEADERS', '')),
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => false,
+
+];
